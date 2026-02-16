fbf69875f2ca650f052a2fac8bd6a3851db0a596
Mark <pupok@gmail.com>
2026-02-16 10:01:41 +0300

    Вынес общую логику из исполняемого файла в функцию

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
-import getName from '../src/cli.js'
+import helloAndGetName from '../src/cli.js'
 import even from '../src/even.js'
 
-console.log(`Welcome to the Brain Games!`)
-
-const name = getName()
-console.log(`Hello, ${name}!`)
+const name = helloAndGetName()
 
 console.log(`Answer "yes" if the number is even, otherwise answer "no".`)
 
